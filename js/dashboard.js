09e2935bc136633ffdca7b5cc2fd0fe8f70a3e39
Yeison <yeison9713>
2018-10-01 17:46:59 -0500

    cambios Y´O

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,30 +1,51 @@
 $nro_paginacion = 1;
 $(document).ready(function () {
+    cargarRegistros();
+
+    $('#paginaAtras').click(function () {
+        $(".loader-spinner").toggle();
+        $('#registroServicios tbody').html('');
+        if ($nro_paginacion === 1) {
+            $nro_paginacion = 1;
+        } else {
+            $nro_paginacion = $nro_paginacion - 1;
+        }
+        cargarRegistros();
+    });
+
+    $('#paginaDelante').click(function () {
+        $(".loader-spinner").toggle();
+        $('#registroServicios tbody').html('');
+        $nro_paginacion++;
+        cargarRegistros();
+    });
+});
+
+function cargarRegistros() {
     datos = "nro_pagina=" + $nro_paginacion;
+    $('.historial-registros label').eq(0).html($nro_paginacion);
     $.ajax({
         async: true,
         type: "POST",
         data: datos,
-        url: 'https://80.211.145.146/barber/inc/cargarRegistros.php',
+        url: 'inc/cargarRegistros.php',  //'https://80.211.145.146/barber/inc/cargarRegistros.php',
         success: function (data) {
-            console.log(data);
             $(".loader-spinner").toggle();
             $registros = $.parseJSON(data);
-            console.log($registros);
             if ($registros.length != 0) {
                 for (var $i = 0; $i < $registros.length; $i++) {
                     $('#registroServicios tbody').append(
                         '<tr>' +
-                            '<td><a class="txt-weight-500">'+$registros[$i]['Servicio']+'</a></td>' +
-                            '<td><a class=" txt-azul">'+$registros[$i]['Barbero']+'</a></td>' +
-                            '<td>'+$registros[$i]['Fecha']+'</td>' +
-                            '<td><a class="txt-weight-600">$'+$registros[$i]['Valor']+'</a></td>' +
+                        '<td><a class="txt-weight-500">' + $registros[$i]['Servicio'] + '</a></td>' +
+                        '<td><a class=" txt-azul">' + $registros[$i]['Barbero'] + '</a></td>' +
+                        '<td>' + $registros[$i]['Fecha'] + '</td>' +
+                        '<td><a class="txt-weight-600">$' + numeral($registros[$i]['Valor']).format('0,0') + '</a></td>' +
                         '</tr>'
                     );
                 }
             } else {
-                $("#alert-movimiento").toggle();                
+                $("#alert-movimiento").toggle();
             }
         }
     });
-});
+}
