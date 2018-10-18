7369a459ed5ef064359e4a9fc170a2f215faf1ca
diegotorres <diegotorres010@hotmail.com>
2018-10-18 15:12:04 -0500

    diego cambio nombres de servicios a ventas

--- js/registroEmpleados.js
+++ js/registroEmpleados.js
@@ -4,7 +4,7 @@ alertCorrecto = { titulo: "¡Correcto!", href: '#registrarServicios', tipo: 'cor
 
 $(document).ready(function () {
     //Cargar barberos
-    ajax( /*'inc/cargarBarberos.php'*/ 'http://80.211.145.146/barber/inc/cargarBarberos.php', null, cargarBarberos);
+    ajax( 'inc/cargarBarberos.php' /*'http://80.211.145.146/barber/inc/cargarBarberos.php'*/, null, cargarBarberos);
 
     cargarFecha();
     $('select[name="servicios"]').change(function () {
