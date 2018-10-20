dc5cb85ebca2dd9c2694c161173cb37473f73932
jhohansilva <jhohanf.silva@gmail.com>
2018-10-20 14:08:11 -0500

    Fix popup btn close

--- js/pop-up.js
+++ js/pop-up.js
@@ -1,11 +1,20 @@
 $(function () {
     $(document).on('click', '[data-popup-btn]', function (event) {
-        var elemento = $(this).data().popupBtn;
-        if (!$('#' + elemento + '').is(':visible')) {
-            $('#' + elemento + '').css({
+        var elemento = '#' + $(this).data().popupBtn;
+        if (!$(elemento).is(':visible')) {
+            $(elemento).css({
                 'display': 'flex',
                 'opacity': '0'
-            }).animate({ opacity: 1 });
+            }).animate({ opacity: 1 }, "fast", function () {              
+                $('.box-top > .icon-close').remove();
+                var box_top = $(elemento).find('.box-top');
+                console.log(box_top);
+                box_top.append('<i class="icon-close material-icons float-right md-22 txt-default" data-popup-btn-close="registroServicio">highlight_off</i>');
+                $('.box-top > .icon-close').css({
+                    'margin-top' : '6px',
+                    'cursor' : 'pointer'
+                });
+            });
         }
 
         document.getElementById('touch').removeEventListener('touchstart', touchstart);
@@ -15,8 +24,8 @@ $(function () {
     });
 
     $(document).on('click', '[data-popup-btn-close]', function (event) {
-        var elemento = $(this).data().popupBtnClose;
-        $('#' + elemento + '').fadeToggle();
+        var elemento = '#' + $(this).data().popupBtnClose;
+        $(elemento).fadeToggle('fast');
 
         document.getElementById('touch').addEventListener('touchstart', touchstart);
         document.getElementById('touch').addEventListener('touchmove', touchmove);
