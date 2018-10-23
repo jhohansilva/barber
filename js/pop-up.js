ca807878a1f7dfdd537aeec862224be5e339f5aa
Jhohan Silva <jhohanf.silva@gmail.com>
2018-10-23 09:52:56 -0500

    Popup responsive

--- js/pop-up.js
+++ js/pop-up.js
@@ -1,30 +1,50 @@
 $(function () {
     $(document).on('click', '[data-popup-btn]', function (event) {
         var elemento = '#' + $(this).data().popupBtn;
-        if (!$(elemento).is(':visible')) {
-            $(elemento).css({
-                'display': 'flex',
-                'opacity': '0'
-            }).animate({ opacity: 1 }, "fast", function () {              
+
+        console.log($(window).width());
+        if ($(window).width() < 426) {
+            $(elemento).toggleClass('close', function () {
                 $('.box-top > .icon-close').remove();
-                var box_top = $(elemento).find('.box-top');                
+                var box_top = $(elemento).find('.box-top');
                 box_top.append('<i class="icon-close material-icons float-right md-22 txt-default" data-popup-btn-close="' + elemento + '">highlight_off</i>');
                 $('.box-top > .icon-close').css({
-                    'margin-top' : '6px',
-                    'cursor' : 'pointer'
+                    'margin-top': '6px',
+                    'cursor': 'pointer'
                 });
             });
+        } else {
+            if (!$(elemento).is(':visible')) {
+                $(elemento).css({
+                    'display': 'flex',
+                    'opacity': '0',
+                    'height': '100%'
+                }).animate({ opacity: 1 }, "fast", function () {
+                    $('.box-top > .icon-close').remove();
+                    var box_top = $(elemento).find('.box-top');
+                    box_top.append('<i class="icon-close material-icons float-right md-22 txt-default" data-popup-btn-close="' + elemento + '">highlight_off</i>');
+                    $('.box-top > .icon-close').css({
+                        'margin-top': '6px',
+                        'cursor': 'pointer'
+                    });
+                });
+            }
         }
+
 
         document.getElementById('touch').removeEventListener('touchstart', touchstart);
         document.getElementById('touch').removeEventListener('touchmove', touchmove);
         PullToRefresh.destroyAll();
+        $('html').css('overflow', 'hidden');
 
     });
 
     $(document).on('click', '[data-popup-btn-close]', function (event) {
-        var elemento = $(this).data().popupBtnClose;        
-        $(elemento).fadeToggle('fast');
+        var elemento = $(this).data().popupBtnClose;
+        
+        if ($(window).width() < 426) $(elemento).toggleClass('close');
+        else $(elemento).fadeToggle('fast');
+
 
         document.getElementById('touch').addEventListener('touchstart', touchstart);
         document.getElementById('touch').addEventListener('touchmove', touchmove);
